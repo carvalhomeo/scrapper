3eaa5653e8409985db1516c2c245058f688dad58
Marcel Carvalho <48029185+carvalhomeo@users.noreply.github.com>
2022-09-28 23:50:14 +0100

    Fixed issue with chromium

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,18 +2,84 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import pupeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
 
 type Data = {
   name: string;
   status: string;
 };
 
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath;
+
+  if (!executablePath) {
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+  }
+  return chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const result = await scrap();
-  res.status(200).json(result);
+  // const result = await scrap();
+  // res.status(200).json(result);
+
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await getBrowserInstance();
+
+    let page = await browser.newPage();
+
+    await page.goto("https://www.playlostark.com/en-gb/support/server-status");
+
+    await page.click(
+      "body > main > section > div > div.ags-ServerStatus-content-tabs > a:nth-child(3) > div"
+    );
+
+    result = await page.evaluate(() => {
+      var listNames = Array.from(
+        document.querySelectorAll(
+          ".ags-ServerStatus-content-responses-response-server"
+        )
+      ).map((element) => ({
+        name: element
+          .querySelector(
+            ".ags-ServerStatus-content-responses-response-server-name"
+          )
+          ?.textContent?.replace("\n", "")
+          .replace("+", "")
+          .trim(),
+        status: element.querySelector(
+          ".ags-ServerStatus-content-responses-response-server-status.ags-ServerStatus-content-responses-response-server-status--good"
+        )
+          ? "good"
+          : "bad",
+      }));
+      return listNames;
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+
+    res.status(200).json(result);
+  }
 }
 
 const scrap = async () => {
