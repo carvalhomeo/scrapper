5afe6feca6f223c9b30f7bf4ba4765919573942c
Marcel Carvalho <48029185+carvalhomeo@users.noreply.github.com>
2022-09-29 14:59:24 +0100

    deleted commented code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
-import pupeteer from "puppeteer";
 import chromium from "chrome-aws-lambda";
-
-type Data = {
-  name: string;
-  status: string;
-};
 
 async function getBrowserInstance() {
   const executablePath = await chromium.executablePath;
@@ -33,9 +26,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // const result = await scrap();
-  // res.status(200).json(result);
-
   let result = null;
   let browser = null;
 
@@ -81,39 +71,3 @@ export default async function handler(
     res.status(200).json(result);
   }
 }
-
-const scrap = async () => {
-  const browser = await pupeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.playlostark.com/en-gb/support/server-status");
-
-  await page.click(
-    "body > main > section > div > div.ags-ServerStatus-content-tabs > a:nth-child(3) > div"
-  );
-
-  const names = await page.evaluate(() => {
-    var listNames = Array.from(
-      document.querySelectorAll(
-        ".ags-ServerStatus-content-responses-response-server"
-      )
-    ).map((element) => ({
-      name: element
-        .querySelector(
-          ".ags-ServerStatus-content-responses-response-server-name"
-        )
-        ?.textContent?.replace("\n", "")
-        .replace("+", "")
-        .trim(),
-      status: element.querySelector(
-        ".ags-ServerStatus-content-responses-response-server-status.ags-ServerStatus-content-responses-response-server-status--good"
-      )
-        ? "good"
-        : "bad",
-    }));
-    return listNames;
-  });
-
-  await browser.close();
-
-  return names;
-};
