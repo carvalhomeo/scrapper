fc5e23adfc0d761572718b1665cefa8cbc4a1a39
Marcel Carvalho <48029185+carvalhomeo@users.noreply.github.com>
2022-09-30 13:22:35 +0100

    changed puppeteer import

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import chromium from "chrome-aws-lambda";
-import pup from "puppeteer-core";
+import puppeteer from "puppeteer-core";
 
 async function getBrowserInstance() {
   const executablePath = await chromium.executablePath;
 
   if (!executablePath) {
-    const puppeteer = require("puppeteer");
     return puppeteer.launch({
       args: chromium.args,
       headless: true,
